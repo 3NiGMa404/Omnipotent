93a899ef91fa5bfedc3a30aec15df7873740a546
3NiGMa404 <53730248+3NiGMa404@users.noreply.github.com>
2024-06-26 16:21:18 +1000

    Add files via upload

--- inout.py
+++ inout.py
@@ -1,13 +1,17 @@
+'''
 import pyttsx3
 engine = pyttsx3.init()
 voices=engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 engine.setProperty('rate',190)
+'''
 def ret(text):
+    '''
     engine.say(text) 
     engine.runAndWait()
     print(text)
-
+    '''
+    print(text)
 import colorama
 def git():                            #Replace contents of git with whatever you want!
     it=input(colorama.Fore.YELLOW+'>: ')  
--- reset.py
+++ reset.py
@@ -1,9 +1,13 @@
 import shutil
 import os
-shutil.rmtree('Memory')
 shutil.rmtree('Info')
-os.mkdir('Memory')
 os.mkdir('Info')
+os.makedirs('Info/OPINIONS')
 os.remove('thoughts.txt')
 op=open('thoughts.txt','w+')
 op.close()
+os.remove('memory_file.py')
+op2=open('memory_file.py','w+')
+op2.write('memory_dict={}')
+op2.close()
+
